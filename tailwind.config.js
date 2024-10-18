14db857e86b8f6b3ea364c922b788b67eecd070c
mos3sVitwo <smoses@vitwo.in>
2024-10-18 09:52:54 +0530

    added jit to fetch classes in real time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './public/**/*.{js,jsx,ts,tsx,vue}'],
   darkMode: 'selector',
   content: ['./public/index.html', './public/script.js'], // Adjust paths as needed
   theme: {
